3d4f788af327e16d8e3dc77e199dece5cb1ae6c2
soheel <soheelsaig0966@gmail.com>
2020-05-07 23:14:26 +0200

    Updates

---
+++ precache-manifest.88a8f31ce5c35b0be788af30a5433aac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52d188ac415c62ac4ff104b68cd181c",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "87c7fd301d436a3adfc9",
+    "url": "/movie-app/static/css/main.5ef7d8eb.chunk.css"
+  },
+  {
+    "revision": "39b7778da7b88b40335a",
+    "url": "/movie-app/static/js/2.607174b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app/static/js/2.607174b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c7fd301d436a3adfc9",
+    "url": "/movie-app/static/js/main.276aacc2.chunk.js"
+  },
+  {
+    "revision": "eb2afba229eb5426c33e",
+    "url": "/movie-app/static/js/runtime-main.6de98e8b.js"
+  },
+  {
+    "revision": "b7ca6374249ec9b79bd995b733af0d3b",
+    "url": "/movie-app/static/media/kisspng-photographic-film-movie-camera-cinema-website-and-mobile-application-development-service-5d3fc9250ede49.9926975015644613490609.b7ca6374.png"
+  },
+  {
+    "revision": "f1c75c5ee3b4eefb58bc97db1d573476",
+    "url": "/movie-app/static/media/movie.f1c75c5e.jpg"
+  }
+]);
